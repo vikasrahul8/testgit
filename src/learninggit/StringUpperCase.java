15a6c0313a5b0085659bb3ff60fe950aeb7b78b3
vikasrahul8 <vikasrahul8@gmail.com>
2019-05-28 00:18:27 +0530

    Ram Added

--- src/learninggit/StringUpperCase.java
+++ src/learninggit/StringUpperCase.java
@@ -2,11 +2,16 @@ package learninggit;
 
 class StringUppercase1{
     public static void main(String as[]){
-         String str="krishna";
-         for(int i=0;i<str.length();i++){
-	   char ch=(char)(str.charAt(i)-32);
+         String str1="krishna";
+         String str2="Ram";
+         for(int i=0;i<str1.length();i++){
+	   char ch=(char)(str1.charAt(i)-32);
 	    System.out.print(ch);
          }
+         for(int i=0;i<str2.length();i++){
+      	   char ch=(char)(str2.charAt(i)-32);
+      	    System.out.print(ch);
+               }
 }
 }	
 
